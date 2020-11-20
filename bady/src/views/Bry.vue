937d27794a4723069c141fef35e344ab81911e75
DESKTOP-DS251IP\K <993953330@qq.com>
2020-11-20 16:25:01 +0800

    no message

--- bady/src/views/Bry.vue
+++ bady/src/views/Bry.vue
@@ -1,13 +1,14 @@
 <template>
-    <div>
-        这是bry的分支
-    </div>
+  <div>
+    这是bry的分支
+  </div>
+  <span>16点23分</span>
 </template>
 
 <script>
-    export default {
-        name: "Bry"
-    }
+export default {
+  name: "Bry"
+}
 </script>
 
 <style scoped>
